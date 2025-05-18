5e0886ef3c69a8d5347b140e6ce0251f4c78f77f
Bantu-art <brianbaraza40@gmail.com>
2025-05-18 11:41:16 +0300

    Update heading color in Index component to blue

--- 01talentweb/frontend/src/pages/Home/Index.jsx
+++ 01talentweb/frontend/src/pages/Home/Index.jsx
@@ -2,7 +2,7 @@ export default function Index(props) {
     console.log(props)
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <h1 className="text-6xl text-gray-400 font-extrabold text-center">
+            <h1 className="text-6xl text-blue-400 font-extrabold text-center">
                 Hello 01 Talent Kenya
             </h1>
         </div>
