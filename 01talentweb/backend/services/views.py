fbb5f0f38c3ac78ccd549c9ab3f4c07f1d6d652b
Bantu-art <brianbaraza40@gmail.com>
2025-05-20 15:11:18 +0300

    feat: implement services view to display all services with details

--- 01talentweb/backend/services/views.py
+++ 01talentweb/backend/services/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from inertia import render
+from talent.models import Service
 
-# Create your views here.
+@inertia('Services/Index')
+def services(request):
+    services = Service.objects.all()
+    service_list = []
+    for service in services:
+        service_data = {
+            'id': str(service.id),
+            'name': service.name,
+            'description': service.description,
+            'icon': service.icon,
+            'image': request.build_absolute_uri(service.image.url) if service.image else None
+        }
+        service_list.append(service_data)
+    return render(request, {
+        'services': service_list
+    })
