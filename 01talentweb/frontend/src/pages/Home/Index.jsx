fc306ca04da9301ea8557bdbd8e401deaf7e2fb1
Philip <oumaphilip01@gmail.com>
2025-05-21 12:15:17 +0300

    use cards to render talents

--- 01talentweb/frontend/src/pages/Home/Index.jsx
+++ 01talentweb/frontend/src/pages/Home/Index.jsx
@@ -2,13 +2,60 @@ import { usePage } from '@inertiajs/react';
 import Hero from './components/Hero.jsx';
 import FeaturedProfiles from './components/FeaturedProfiles.jsx';
 
+
+const sampleTalents = [
+    {
+      id: 1,
+      name: 'Joeylene Rivera',
+      image: '/images/talents/joeylene.jpg',
+      skills: ['Golang', 'Docker', 'RESTful APIs', 'Database Design'],
+      average_rating: 4.8,
+    },
+    {
+      id: 2,
+      name: 'Amara Njeri',
+      image: '/images/talents/amara.jpg',
+      skills: ['Python', 'Data Analysis', 'Pandas', 'Jupyter'],
+      average_rating: 4.5,
+    },
+    {
+      id: 3,
+      name: 'Ezekiel Mwangi',
+      image: '/images/talents/ezekiel.jpg',
+      skills: ['React', 'Tailwind CSS', 'Next.js', 'TypeScript'],
+      average_rating: 4.9,
+    },
+    {
+      id: 4,
+      name: 'Fatima Khalid',
+      image: '/images/talents/fatima.jpg',
+      skills: ['UX Design', 'Figma', 'Accessibility', 'Design Systems'],
+      average_rating: 4.6,
+    },
+    {
+      id: 5,
+      name: 'Brian Ochieng',
+      image: '/images/talents/brian.jpg',
+      skills: ['Java', 'Spring Boot', 'MySQL', 'APIs'],
+      average_rating: 4.7,
+    },
+    {
+      id: 6,
+      name: 'Zainab Yusuf',
+      image: '/images/talents/zainab.jpg',
+      skills: ['Cybersecurity', 'Linux', 'Networking', 'Ethical Hacking'],
+      average_rating: 4.4,
+    },
+  ];
+
+  
 export default function Index() {
     const { talents = [] } = usePage().props;
 
     return (
         <div className="min-h-screen">
             <Hero />
-            <FeaturedProfiles talents={talents} />
+            <FeaturedProfiles talents={sampleTalents} />
         </div>
     );
 }
