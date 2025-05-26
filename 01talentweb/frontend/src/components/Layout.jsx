c19193985c6dae658d3491d08602a5f81ddd6387
rotieno <rabinyitzahck@gmail.com>
2025-05-26 21:32:51 +0300

    fix: resolve Layout.jsx conflict

--- 01talentweb/frontend/src/components/Layout.jsx
+++ 01talentweb/frontend/src/components/Layout.jsx
@@ -1,16 +1,22 @@
 import { Link } from "@inertiajs/react";
+import Footer from "@/components/Footer.jsx";
+
+import Navbar from '@/components/Navbar.jsx';
+import Hero from '@/components/Hero.jsx';
+import Stats from '@/components/Stats.jsx';
+import CompanyLogos from '@/components/CompanyLogos.jsx';
 
 const Layout = ({children}) => (
 <>
-<nav className="border border-gray-400 mb-2 flex justify-center items-center">
-       <ul className="flex gap-2">
-              <li><Link href="/">Home</Link></li>
-              {/* <li><Link href="/services">Services</Link></li> */}
-              {/* <li><Link href="/contact">Contact</Link></li> */}
-       </ul>
-</nav>
-<main className="flex justify-center items-center">
+<main className="w-full h-screen overflow-hidden overflow-y-scroll">
+       {/* <nav className="border border-gray-400 mb-2 flex justify-center items-center">
+              <ul className="flex gap-2">
+                     <li><Link href="/">Home</Link></li>
+                     <li><Link href="/contact">Contact</Link></li>
+              </ul>
+       </nav> */}
        { children }
+       <Footer />
 </main>
 </>
 )
