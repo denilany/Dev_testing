6c4e3d76d45b70358b4b277c21e3971963ee7b07
Bantu-art <brianbaraza40@gmail.com>
2025-05-28 09:31:40 +0300

    fix: add missing newline at end of file in urls.py

--- 01talentweb/backend/talent/urls.py
+++ 01talentweb/backend/talent/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'talent'
 urlpatterns = [
     path('', views.talents_list, name='talents_list'),
+    path('featured/', views.featured_developers, name='featured_developers'),
 ]
