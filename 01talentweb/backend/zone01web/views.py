fddcdece1af2069e5a92d2516a00de6058fc56cb
Bantu-art <brianbaraza40@gmail.com>
2025-05-28 11:39:06 +0300

    feat: implement index view to retrieve and format featured developers

--- 01talentweb/backend/zone01web/views.py
+++ 01talentweb/backend/zone01web/views.py
@@ -1,7 +1,27 @@
 from inertia import inertia
 from inertia import render
+from talent.models import Talent
+from django.utils import timezone
+from django.db.models import Q
+import random
 
 @inertia('Home/Index')
 def index(request):
-    # return render(request, template_name="base.html")
-    return {}
+    # Get all talents with featured_order
+    all_devs = Talent.objects.filter(featured_order__isnull=False).order_by('featured_order')
+    
+    # Format the data to match what the frontend expects
+    formatted_devs = []
+    for dev in all_devs:
+        skills = dev.profile.get('skills', [])
+        formatted_devs.append({
+            'id': str(dev.id),
+            'name': dev.name,
+            'image': f'/images/talents/{dev.name.lower().replace(" ", "_")}.jpg',
+            'skills': skills,
+            'average_rating': dev.profile.get('average_rating', 4.5)
+        })
+    
+    return {
+        'featured_developers': formatted_devs
+    }
