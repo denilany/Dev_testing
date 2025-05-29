97a63494148a8dc74554dc15c2e4c2b52cc46442
rabinnnn <otiinibar@gmail.com>
2025-05-29 12:56:39 +0300

    styles: resize image mask

--- 01talentweb/frontend/src/components/HiringModel.jsx
+++ 01talentweb/frontend/src/components/HiringModel.jsx
@@ -74,17 +74,17 @@ const HiringModel = () => {
   ];
 
   return (
-    <section className="bg-white w-full py-12 px-4 md:px-16 relative overflow-visible">
+    <section className="bg-white w-full ultra-large:mb-10 py-12 px-4 md:px-16 relative overflow-visible">
       <Container>
         <div className="sticky top-20 self-start bg-white py-4 z-20">
-      <h2 className=" text-2xl md:text-3xl font-bold text-center mb-10 ultra-large:text-h2 ultra-large:pb-20">
+      <h2 className=" text-2xl md:text-3xl font-bold text-center mb-10 ultra-large:text-h2 ultra-large:pb-18">
         WHY <span className="text-blue-600">OUR HIRING MODEL</span> WORKS FOR YOU
       </h2>
         </div>
 
       <div 
       
-      className="grid md:grid-cols-2 max-w-screen mx-auto gap-10 ">
+      className="grid md:grid-cols-2 max-w-screen  mx-auto gap-10">
         {/* Left Scrollable Column */}
         <div className="space-y-10" >
           {features.map((item, i) => (
@@ -98,43 +98,53 @@ const HiringModel = () => {
         {/* Right Static Image */}
         <div 
          ref={imageRef} 
-        className="sticky top-24 self-start z-10"
+        className="sticky top-24 self-start z-10 mb-20"
         
         >
+           {/* Top Left Chevron */}
+           <img
+            src="/static/images/arrows.svg"
+            alt="Chevron Top Left"
+            className="absolute top-4 left-0 w-5 md:w-15 z-30 ultra-large:w-[60px]"
+          />
+
           {/* Brush mask */}
           <img
             src="/static/images/brush-mask.png"
             alt="Brush"
-            className="absolute z-20 top-0 left-0 w-full ultra-large:-top-8 object-contain pointer-events-none"
+            className="absolute z-10 top-10 left-0 ultra-large:top-20  ultra-large:h-[500px] object-contain pointer-events-none"
           />
 
           {/* Main Image */}
           <img
             src="/static/images/hiring_model_image.png"
             alt="Hiring Visual"
-            className="relative z-10 w-full top-0 left-0 w-full h-full rounded shadow-md object-cover"
+            className="relative z-0 ultra-large:w-[620px] top-0 ultra-large:top-24  ultra-large:h-[480px] left-0 ultra-large:left-12 rounded shadow-md object-cover"
           />
 
-          {/* Top Left Chevron */}
-          <img
-            src="/static/images/arrows.svg"
-            alt="Chevron Top Left"
-            className="absolute top-4 left-4 w-5 md:w-15 z-30"
-          />
 
           {/* Bottom Right Chevron */}
           <img
             src="/static/images/arrows.svg"
             alt="Chevron Bottom Right"
-            className="absolute bottom-4 right-4 w-5 md:w-15 z-30"
+            className="absolute -bottom-40 right-20 w-5 md:w-15 z-30 ultra-large:w-[60px]"
           />
+
+         
+
+
+          <img 
+          src="/static/images/dots.svg" 
+          alt="dots" 
+          className=" absolute -bottom-40 left-4 z-30 pt-10 w-2 ultra-large:w-40" />
+
         </div>
       </div>
 
       {/* Bottom Decorative Dots */}
-      <div className="flex justify-center mt-12">
-        <img src="/static/images/dots.svg" alt="dots" className="w-20" />
-      </div>
+      {/* <div className="flex left-0 mt-12">
+        <img src="/static/images/dots.svg" alt="dots" className="w-20 ultra-large:w-40" />
+      </div> */}
     </Container>
     </section>
   );
