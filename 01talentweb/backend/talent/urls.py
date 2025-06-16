ae6b35ad9eee56e6a457554fe3ad11b2469fff3d
MeFerdi <ferdinandodhis254@gmail.com>
2025-06-16 12:16:16 +0300

    fix: add talent_detail URL pattern for individual talent pages

--- 01talentweb/backend/talent/urls.py
+++ 01talentweb/backend/talent/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'talent'
-
 urlpatterns = [
     path('', views.talent_page, name='talent'),
+    path('<uuid:talent_id>/', views.talent_detail, name='talent_detail'),
 ]
